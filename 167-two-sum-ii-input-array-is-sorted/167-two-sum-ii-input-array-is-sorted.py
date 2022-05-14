4e321592f81d913f36f371faebfbb42d18231978
amohamed93 <60699357+amohamed93@users.noreply.github.com>
2022-05-14 14:32:08 +0200

    Time: 236 ms (18.47%), Space: 15 MB (43.09%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,11 +1,8 @@
-import numpy as np
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for each_el in set(numbers):
             if target - each_el in numbers and target - each_el != each_el:
                 return sorted([numbers.index(each_el) +1, numbers.index( target - each_el)+1])
             elif target - each_el == each_el:
-                values = np.array(numbers)
-                searchval = each_el
-                ii = sorted([x + 1 for x in np.where(values == searchval)[0][:2]])
-                return ii
+                return [index + 1 for index, num in enumerate(numbers) if num == each_el]
+                
