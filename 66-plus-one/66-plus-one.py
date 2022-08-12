28e61e143694682f717d77308c841a96c50313af
amohamed93 <60699357+amohamed93@users.noreply.github.com>
2022-08-12 15:36:27 +0200

    Time: 51 ms (51.71%), Space: 13.8 MB (96.68%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -2,5 +2,5 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         number = ''.join([str(digit)for digit in digits])
         number = int(number) + 1
-        number = [int(digit) for digit in list(str(number))]
+        number = list(str(number))
         return number
