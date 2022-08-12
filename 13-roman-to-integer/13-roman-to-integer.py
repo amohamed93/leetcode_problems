5b758de9408ca1991470205405cd50f5662592d2
amohamed93 <60699357+amohamed93@users.noreply.github.com>
2022-08-12 14:34:59 +0200

    Time: 69 ms (64.31%), Space: 14 MB (31.19%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        convert_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        index = 0
+        number = 0
+        for each_char in list(s):
+            if index > len(s) - 1:
+                break
+            value = convert_dict[s[index]]
+            print("value", value)
+            if index + 1 < len(s) and convert_dict[s[index]] < convert_dict[s[index+1]]:
+                print("enter here",  convert_dict[each_char])
+                value = -1 * value + convert_dict[s[index+1]]
+                index += 1
+                
+            number += value
+            print("number", number)
+            index += 1
+        return number
